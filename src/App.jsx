7d41384fb86f5e63d519a40c3355ff10f2b9632f
piyushsoni975 <piyushkrsoni975@gmail.com>
2024-11-11 00:36:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Hero from './component/Hero';
+import Navigation from './component/Navigatin';
+const App = () => {
+  return <div>
+    <Navigation/>
+    <Hero/>
+  </div>;
+};
+
+export default App;
---
+++ src/component/Hero.jsx
@@ -0,0 +1,32 @@
+const Hero =()=>{
+    return (
+        <main className="hero container">
+           <div className="hero-section">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>
+             YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+            </p>
+           
+           <div className="hero-btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+           </div>
+           <div className="shopping">
+            <p>Also Avalable On</p>
+            <div className="brand-img">
+                <img src="/amazon.png" alt="amazon-logo" />
+                <img src="flipkart.png" alt="flipkart-logo" />
+            </div>
+           </div>
+           </div>
+           <div className="hero-img">
+            <img src="hero-image.png" alt="hero-logo" />
+           </div>
+        </main>
+    )
+
+  
+
+   
+}
+export default Hero;
